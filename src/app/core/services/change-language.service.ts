484ccfa6fc1a42ee2b3c7cc0fba4a346d2121ab3
atarek <ahmad_tarek_abdalla2096@outlook.com>
2022-03-16 19:58:18 +0200

    add language lService

---
+++ src/app/core/services/change-language.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChangeLanguageService {
+  langauge: any = new BehaviorSubject<any>('en')
+  constructor() {
+    if (localStorage.getItem('lang') == 'ar') {
+      this.langauge.next('ar')
+    } else {
+      this.langauge.next('en')
+    }
+
+
+  }
+
+}
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangeLanguageService } from 'src/app/core/services/change-language.service';
 import { SidebarService } from 'src/app/core/services/sidebar.service';
 import { ThemsService } from 'src/app/services/style/thems.service';
 ;
@@ -14,7 +15,9 @@ export class HeaderComponent implements OnInit {
   darkMood: boolean = false;
   constructor(
     public sideBarService: SidebarService,
-    public themeService: ThemsService
+    public themeService: ThemsService,
+    public languageService: ChangeLanguageService,
+
 
   ) { }
 
@@ -27,7 +30,22 @@ export class HeaderComponent implements OnInit {
     this.sideBarService.changeSideBar()
   }
 
+  changeLanguage() {
+    if (localStorage.getItem('lang') == 'ar') {
+      localStorage.setItem('lang', 'en')
+      this.languageService.langauge.next('en')
+    } else if (localStorage.getItem('lang') == 'en') {
+      localStorage.setItem('lang', 'ar')
+      this.languageService.langauge.next('ar')
+    }
 
+    document
+      .querySelector('body')!
+      .setAttribute('dir', localStorage.getItem('lang') == 'ar' ? 'rtl' : 'ltr')
+    document
+      .querySelector('html')!
+      .setAttribute('lang', localStorage.getItem('lang') == 'ar' ? 'ar' : 'en')
+  }
 
 
   changeTheme() {
