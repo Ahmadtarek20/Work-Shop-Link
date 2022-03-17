ef942ab290f6de416e874cacdb037ca076ce15e1
atarek <ahmad_tarek_abdalla2096@outlook.com>
2022-03-17 18:17:04 +0200

    fix the box baners data to aline

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -8,45 +8,20 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 ;
-import { tap } from 'rxjs/operators';
-import { Router } from '@angular/router';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router,) { }
+  constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (request.url.includes('maps.google')) {
-      return next.handle(request);
-    }
-    const token = localStorage.getItem('token') || '';
     let requestWithAuth: any;
     const lang = localStorage.getItem('lang') || '';
-    if (token) {
-      requestWithAuth = request.clone({
-        headers: request.headers
-          .set('Authorization', `Bearer ${token}`)
-          .set('accept-language', lang)
-          .set('Accept', '*/*')
-      });
-    } else {
-      requestWithAuth = request.clone({
-        headers: request.headers
-          .set('accept-language', lang)
-          .set('Accept', '*/*')
-      });
-      return next.handle(requestWithAuth).pipe(
-        tap((e) => { },
-          (error) => {
-            if (error.status == 401) {
-              this.router.navigate(['/auth', 'login']);
-            }
-          })
-      )
-    }
-
-
+    requestWithAuth = request.clone({
+      headers: request.headers
+        .set('accept-language', lang)
+        .set('Accept', '*/*')
+    });
     return next.handle(requestWithAuth);
   }
 
